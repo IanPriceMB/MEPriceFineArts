ad8e3bc3783afc30446514c59a7c9bf53201e90a
Ian Price <ianpriceve@gmail.com>
2018-10-02 22:52:33 -0600

    some validation on the schema and a little more discription in my API

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,11 +6,25 @@ const Product = require('../models/product')
 router.get('/', (req, res, next) => {
     //where and limit to select further see docs
     Product.find()
+    .select(' name price _id')
     .exec()
     .then(docs => {
-        console.log(docs);
+        const response = {
+            count: docs. length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name, 
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/'+doc._id
+                    }
+                }
+            })
+        }
         // if (docs.length >= 0){
-        res.status(200).json(docs);
+        res.status(200).json(response);
         // } else {
         //     res.status(404).json({
         //         message: 'no entries found'
@@ -33,12 +47,26 @@ router.post('/', (req, res, next) => {
     });
     product.save().then( result => {
         console.log(result);
+        res.status(201).json({
+            message: 'Created product',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/'+result._id
+                }
+            }
+        });
     })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        });
     });
+    
 });
 
 router.get('/:productId', (req, res, next) => {
