dfdb811b851651832fbbf93fbfabd463b46e8b7f
Ian Price <ianpriceve@gmail.com>
2018-12-13 17:02:37 -0700

    gallery basic functionality and basic client side styling done

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,75 +1,73 @@
 import React, {Component} from 'react';
 import API from '../utils/API';
+import './css/MainPage.css';
 
 class MainPage extends Component {
 
-    state = {
-        display: 'Home',
-        images: null
-    };
+  state = {
+    display: 'Home',
+    images: null
+  };
 
-    setBackground = image => {
+  setBackground = image => {
 
-    };
+  };
 
-    setDisplay = location => {
-        this.setState({display: location})
-    };
+  setDisplay = location => {
+    this.setState({display: location})
+  };
 
-    componentDidMount() {
-        this.loadImages();
-    };
+  componentDidMount() {
+    this.loadImages();
+  };
 
-    loadImages = () => {
-        API.getAllProducts()
-        .then(dataWrappedByPromise => dataWrappedByPromise.json())
-        .then(res => {this.setState({images: res.data}); console.log(res);})
-        .catch(err => console.log(err))
-    };
+  loadImages = () => {
+    API.getAllProducts()
+    .then(dataWrappedByPromise => dataWrappedByPromise.json())
+    .then(res => {
+      this.setState({
+        images: res
+      }); 
+      console.log(res);
+    })
+    .catch(err => console.log(err))
+  };
 
-    render(){
-        return(
-            <div>
-                <header>
-                    <h1>M.E. Price Fine Art</h1>
-                </header>
-                <div>
-                    <h2 className="link-item" onClick={() => this.setDisplay('Home')}>Home</h2>
-                    <h2 className="link-item" onClick={() => this.setDisplay('Gallery')}>Gallery</h2>
-                    <h2 className="link-item" onClick={() => this.setDisplay('About')}>About</h2>
-                    <h2 className="link-item" onClick={() => this.setDisplay('Contact')}>Contact</h2>
-                </div>
-                {this.state.display === 'Gallery' ?(
-                    <div>
-                        <ul>
-                            {(this.state.images.products).map((image, i) => (
-                                <li key={i}>{image.name}</li> 
-                            ))}
-                        </ul>
-                        {(this.state.images.products).map((image, i) => (
-                            <img key={i} src={image.request.url} alt='title'/> 
-                        ))}
-                    </div>):(<div></div>)}
-                {this.state.display === 'Home'? (
-                    <button>hit me</button>
-                ):(<div></div>)}
-            </div>
-        )
-        // res different pages based on state
-        // if(this.state.display == 'Home' || null){
-            
-        // }
+  render(){
+    return(
+      <div className="MainPage">
+        <header>
+            <h1>M.E. Price Fine Art</h1>
+            <nav>
+              <h2 onClick={() => this.setDisplay('Home')}>Home</h2>
+              <h2 onClick={() => this.setDisplay('Gallery')}>Gallery</h2>
+              <h2 onClick={() => this.setDisplay('About')}>About</h2>
+              <h2 onClick={() => this.setDisplay('Contact')}>Contact</h2>
+          </nav>
+        </header>
+        {this.state.display === 'Gallery' ?(
+          <div className="Gallery">
+            {(this.state.images.products).map((image, i) => (
+                <img src={image.productImage} alt={image.name} />
+            ))}
+          </div>):(<div></div>)}
+    </div>
+  )
+  // res different pages based on state
+  // if(this.state.display == 'Home' || null){
+      
+  // }
 
-        // if(this.state.display == 'Gallery'){
-            
-        // }
-        // if(this.state.display == 'About'){
-            
-        // }
-        // if(this.state.display == 'Contact'){
-            
-        // }
-    }
+  // if(this.state.display == 'Gallery'){
+      
+  // }
+  // if(this.state.display == 'About'){
+      
+  // }
+  // if(this.state.display == 'Contact'){
+      
+  // }
+  }
 
 }
 export default MainPage;
